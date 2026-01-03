8bd525f2935c823170e0e99947ce6f28ee927d8d
Shadow <manikantakommoju10@gmail.com>
2026-01-03 18:27:07 +0530

    documented new features

--- src/main/java/com/yato/urlShortenerb/config/CorsConfig.java
+++ src/main/java/com/yato/urlShortenerb/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174", "http://localhost:5173")
+                .allowedOrigins("http://localhost:5174", "http://localhost:5173","https://urlshortenerf.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true)
