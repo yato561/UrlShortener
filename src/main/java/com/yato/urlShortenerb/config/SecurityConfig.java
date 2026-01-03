3e310b4347078e042077f5b267c87f8f68f551ba
Shadow <manikantakommoju10@gmail.com>
2026-01-03 21:03:28 +0530

    changes in authentication

--- src/main/java/com/yato/urlShortenerb/config/SecurityConfig.java
+++ src/main/java/com/yato/urlShortenerb/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
+                                "/api/health",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
