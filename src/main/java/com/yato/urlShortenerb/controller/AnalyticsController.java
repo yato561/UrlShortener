409aec8f96a607709e6637701c274b1f236c5b22
Shadow <manikantakommoju10@gmail.com>
2026-01-04 10:18:11 +0530

    changes in analytics

--- src/main/java/com/yato/urlShortenerb/controller/AnalyticsController.java
+++ src/main/java/com/yato/urlShortenerb/controller/AnalyticsController.java
@@ -1,12 +1,11 @@
 package com.yato.urlShortenerb.controller;
 
-
-import com.yato.urlShortenerb.config.JWTUtils;
 import com.yato.urlShortenerb.service.AnalyticsService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,19 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
-    private final JWTUtils jwtUtils;
 
     @GetMapping("/overview")
-    public ResponseEntity<?> getAnalytics(HttpServletRequest request){
-        String auth= request.getHeader("Authorization");
-        if(auth==null || !auth.startsWith("Bearer "))
-            return ResponseEntity.status(401).body("Missing Token");
+    public ResponseEntity<?> getAnalytics() {
 
-        String token= auth.substring(7);
-        String email= jwtUtils.getEmailFromToken(token);
+        Authentication auth =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null || !auth.isAuthenticated()) {
+            return ResponseEntity.status(401).body("Unauthorized");
+        }
+
+        String email = auth.getName();
+
+        log.info("Fetching analytics for {}", email);
 
         return analyticsService.getAnalytics(email);
     }
-
-
 }
